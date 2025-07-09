2997aefdfb59adfc8873a075eca76fd30e3f4b0e
Carl Son <carlsonlim83@gmail.com>
2025-07-09 19:17:17 +0700

    Update script.js

--- script/script.js
+++ script/script.js
@@ -37,3 +37,41 @@ document.querySelectorAll('.card-produk').forEach(card => {
     });
 });
 
+const inputkodepromo = document.querySelector('#kodepromo');
+
+inputkodepromo.addEventListener('input',() => {
+    inputkodepromo.value = inputkodepromo.value.touppercase().replace(/\s+/g,'');
+});
+
+const orderform = document.querySelector('.order section form');
+const output = document.querySelector('#output');
+
+orderform.addEventListener('submit', (e)=> {
+    e.preventDefault();
+    output.computedStyleMap.display = 'block';
+    const outputlist = document.querySelector('#output ol');
+    outputlist.innerHTML = '';
+
+    let total =0;
+    document.querySelectorAll('.card-produk').foreach(card => {
+        const judul = card.querySelector('h3').textContent;
+        const qty = card.querySelector('.quantity input').value;
+        const harga = card.querySelector('.price').textContent.match(/\d+/)[0];
+        const li = document.createElement('li')
+        li.innerHTML = `<p><span id ="judulproduk">${judul}</span><span id= "subtotalharga">$${harga}</span><p>`
+        outputlist.appendChild(li);
+        total += parseInt(harga);
+
+        const totalhargaawal = document.querySelector('#totalhargawal');
+        const totalhargaakhir = document.querySelector('#totalhargakhir');
+
+        totalhargaawal.style.color = 'black';
+        totalhargaawal.style.textdecoration = 'none';
+        totalhargaakhir.style.display = 'none';
+
+        totalhargaawal.innerHTML = `$${total}`;
+        
+    }
+    )
+
+});
